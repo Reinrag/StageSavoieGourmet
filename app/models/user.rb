46f3579fc1a717195ebcc201270637d4b350b237
Reinrag <phenixtristan@gmail.com>
2018-06-15 07:48:01 +0000

    ajout des mdp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: { case_sensitive: false}, length: {minimum: 3, maximum: 25}
-  validates :email, presence: true, uniqueness: { case_sensitive: false}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
+  validates :username, presence: true,
+                        uniqueness: { case_sensitive: false},
+                        length: {minimum: 3, maximum: 25}
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false},
+                    format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
+    has_secure_password
 end
