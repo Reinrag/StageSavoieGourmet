064d20cbd9f196c62623a669cd7d362b116dc21b
Reinrag <phenixtristan@gmail.com>
2018-06-18 08:01:15 +0000

    authentification methode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    
+    before_action :set_user, only: [:edit, :update, :show]
     def new
         @user = User.new
     end
@@ -15,11 +17,9 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Mise à jour avec succès"
             redirect_to edit_user_path
@@ -29,14 +29,19 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id]) 
+        
     end
     
     def index
         @users = User.paginate(:page => params[:page], :per_page => 5)
     end
     
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+    private 
+        def set_user
+            @user = User.find(params[:id]) 
+        end
+        
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 end
