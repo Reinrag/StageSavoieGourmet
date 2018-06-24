35291ebd644e658e58d524cf9de6b68e80310d44
Reinrag <phenixtristan@gmail.com>
2018-06-24 13:02:48 +0000

    Créateur d'article en ligne

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post 'login', to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
   
+  resources :articles
+  
   resources :users, except: [:new] do
     member do
       get 'confirm'
