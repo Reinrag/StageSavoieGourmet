7b8801499f2fbdf796bcf9fb8e47e1a806866d85
Reinrag <phenixtristan@gmail.com>
2018-06-24 21:30:31 +0000

    Assossiation article-utilisateur

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ActiveRecord::Base
+  
+  has_many :articles
+  
+  before_save { self.email.downcase! }
   validates :username, presence: true,
                         uniqueness: { case_sensitive: false},
                         length: {minimum: 3, maximum: 25}
+  
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false},
                     format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
+  
   has_secure_password
+  
   has_secure_token :confirmation_token
+  
   def to_session
     {id: id}
   end
